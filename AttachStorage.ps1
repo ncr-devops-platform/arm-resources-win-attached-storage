c1213a84691ad5cec9d13ced83edffd697fe0400
John Kerry <jk185160@ncr.com>
2017-09-06 16:34:23 -0400

    fixing the xStorage calls for the most recent xStorage api

--- AttachStorage.ps1
+++ AttachStorage.ps1
@@ -6,13 +6,13 @@ Configuration AttachedDisk
   {
     xWaitforDisk Disk2
     {
-      DiskNumber = 2
+      DiskId = 2
       RetryIntervalSec = 20
       RetryCount = 30
     }
     
     xDisk ADDataDisk {
-      DiskNumber = 2
+      DiskId = 2
       DriveLetter = "F"
       DependsOn = "[xWaitForDisk]Disk2"
     }
