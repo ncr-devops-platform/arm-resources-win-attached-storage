e562d3d76bf6a4d227ad469ac4661bee5242974d
John Kerry <jk185160@ncr.com>
2017-09-13 08:05:34 -0400

    Adding executuion policy restrictions

--- AttachStorage.ps1
+++ AttachStorage.ps1
@@ -4,17 +4,33 @@ Configuration AttachedDisk
 
   Node "localhost"
   {
+    Script EnableUnrestrictedExecutionPolicy
+    {
+      SetScript = { Set-ExecutionPolicy -ExecutionPolicy unrestricted -force }
+      GetScript =  { Get-ExecutionPolicy }
+      TestScript = { (Get-ExecutionPolicy) -eq "Unrestricted" }
+    }
+
     xWaitforDisk Disk2
     {
       DiskId = 2
       RetryIntervalSec = 20
       RetryCount = 30
+      DependsOn = "[Script]EnableUnrestrictedExecutionPolicy"
     }
     
     xDisk ADDataDisk {
       DiskId = 2
       DriveLetter = "F"
-      DependsOn = "[xWaitForDisk]Disk2"
+      DependsOn = @("[xWaitForDisk]Disk2", "[Script]EnableUnrestrictedExecutionPolicy")
+    }
+
+    Script EnableRestrictedExecutionPolicy
+    {
+      SetScript = { Set-ExecutionPolicy -ExecutionPolicy Restricted -force }
+      GetScript =  { Get-ExecutionPolicy }
+      TestScript = { (Get-ExecutionPolicy) -eq "Restricted" }
+      DependsOn "[xDisk]ADDataDisk"
     }
   }
 }
